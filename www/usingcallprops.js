7bfeb840d0a96bf66821986c7c86d703ecd22c18
chrisminnick <chris@watzthis.com>
2016-07-06 15:42:16 -0700

    converted number to string

--- www/usingcallprops.js
+++ www/usingcallprops.js
@@ -27,7 +27,7 @@ call(myConfig.numbers[0], {
                 var calledName = say_as(currentCall.calledName,'phone');
                 say("I see that your name is " + calledName);
             }
-            var calledID = say_as(currentCall.calledID,'phone');
+            var calledID = say_as(''+ currentCall.calledID,'phone');
             say("Your caller ID is " + calledID);
         }
 
