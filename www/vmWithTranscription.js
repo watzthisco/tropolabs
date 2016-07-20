479bc3f4e0faa2d7700c46f0c9039487f954b5c3
chrisminnick <chris@watzthis.com>
2016-07-20 15:55:40 -0700

    added config json

--- www/vmWithTranscription.js
+++ www/vmWithTranscription.js
@@ -8,9 +8,13 @@
 //load an external json file with settings.
 var myConfig = JSON.parse(load_json("http://hosting.tropo.com/5055259/www/config/config.json"));
 
+var config = [
+    {"department":"sales","phone":myConfig.numbers[0],"email":myConfig.email},
+    {"department":"service","phone":myConfig.numbers[0],"email":myConfig.email}];
+
 var callerID = currentCall.callerID;
-var result = ask("For sales, press 1. For service, press 2.", {
-    choices: "1,2",
+var result = ask(listSelections(config), {
+    choices: listKeys(config),
     mode: "dtml",
     onChoice: function (result) {
 
@@ -18,37 +22,14 @@ var result = ask("For sales, press 1. For service, press 2.", {
         var userInput = parseInt(result.value);
 
         switch (userInput) {
-            case 1:
-                transfer(myConfig.numbers[1], {
-                    timeout: 15,
-                    playvalue: "http://www.phono.com/audio/holdmusic.mp3",
-                    onTimeout: function () {
-                        say("Thank you for calling sales. We're not available right now.");
-                        record("Please record a message after the beep.", {
-                                beep:true,
-                                maxTime:30,
-                                transcriptionOutURI: "mailto:"+myConfig.email,
-                                transcriptionID:callerID
-                            }
-                        );
-                    }
-                });
+            case 0:
+                transferCall(userInput);
+
                 break;
 
-            case 2:
-                transfer(myConfig.numbers[0], {
-                    timeout: 15,
-                    onTimeout: function () {
-                        say("Thank you for calling service. We're not available right now.");
-                        record("Please record a message after the beep.", {
-                                beep:true,
-                                maxTime:30,
-                                transcriptionOutURI: "mailto:"+myConfig.email,
-                                transcriptionID:callerID
-                            }
-                        );
-                    }
-                });
+            case 1:
+                transferCall(userInput);
+
                 break;
 
             default:
@@ -62,6 +43,39 @@ var result = ask("For sales, press 1. For service, press 2.", {
     }
 });
 
+function transferCall(dept){
+    transfer(config[dept].phone, {
+        timeout: 15,
+        playvalue: "http://www.phono.com/audio/holdmusic.mp3",
+        onTimeout: function () {
+            say("Thank you for calling " + config[dept].department + ". We're not available right now.");
+            record("Please record a message after the beep.", {
+                    beep:true,
+                    maxTime:30,
+                    transcriptionOutURI: "mailto:"+config[dept].email,
+                    transcriptionID:callerID
+                }
+            );
+        }
+    });
+}
+
+function listSelections(config){
+    var sayString = "";
+    for (var i=0; i<config.length; i++){
+        sayString += "Select " + i + " for " + config[i].department + ". ";
+    }
+    return sayString;
+}
+
+function listKeys(config){
+    var arr=[];
+
+    for (var i=0; i<config.length; i++) {
+        arr.push(i);
+    }
+    return arr.join(",");
+}
 
 //file loading function.
 function load_json(url) {
