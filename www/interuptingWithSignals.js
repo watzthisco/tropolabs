42b92d646b35049385fb9bdb01435b157d2df12c
chrisminnick <chris@watzthis.com>
2016-07-21 14:39:12 -0700

    interupting instructions

--- www/interuptingWithSignals.js
+++ www/interuptingWithSignals.js
@@ -1,3 +1,13 @@
+/*
+ * Tropo Learning Lab - Interupting with Signals
+ * Level: Intermediate
+ * To Test:
+ * call 586-232-5981
+ * input the sessionID into this URL: https://api.tropo.com/1.0/sessions/<session-id>/signals
+ * Open the url in a browser.
+ */
+
+
 var sessionid = currentCall.sessionId;
 log("The Session ID is " + sessionid);
 say("http://www.phono.com/audio/holdmusic.mp3", {
