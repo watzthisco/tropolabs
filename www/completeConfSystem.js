9e766483d96b8a5d6ee79cce3f3ee0839d814614
chrisminnick <chris@watzthis.com>
2016-08-16 14:24:51 -0700

    revised and fixed

--- www/completeConfSystem.js
+++ www/completeConfSystem.js
@@ -10,30 +10,43 @@
 // when someone joins or leaves that conference, the attached phone
 // number will get an SMS alerting them.
 
-var pins = {"1234":"14075551212", "1337":"19255556789", "2600":""};
+var pins = {'1234':'19167305777', '1337':'19255556789', '2600':''};
 
-answer();
+while (currentCall.isActive) {
 
-while (currentCall.isActive){
-    var response = ask("Enter your conference ID. Press the pound key when finished.", {
-        choices:"[3-10 DIGITS]",
-        terminator: "#",
-        mode: "keypad",
-        timeout: 8,
-        onBadChoice: function(){
-            say("Sorry, that's not a valid conference ID");
-        },
-        onChoice: function(event){
-            say('<speak>Conference ID <say-as interpret-as="vxml:digits">' + event.value + '</say-as> accepted. You will now be placed into the conference. Press pound to exit without disconnecting. Please announce yourself.</speak>');
-            conference(event.value,{terminator: "#"});
-
-            // Send an alert that someone has left the conference
-            message(currentCall.callerID + ' has left conference ' . event.value, {to: pins[event.value], network: 'SMS'});
+    var response = ask('Enter your conference ID. Press the pound key when finished.',
+        {
+            choices: '[3-10 DIGITS]',
+            terminator: '#',
+            mode: 'keypad',
+            timeout: 8,
+            onBadChoice: function () {
+                say("Sorry, that's not a valid conference ID");
             },
-        onTimeout: function(){
+            onTimeout: function () {
                 say("Sorry, I didn't hear anything");
-        }
+            },
+            onChoice: function (event) {
+                if (!pins[event.value]) {
+
+                    say('Sorry, that is not a valid conference ID.');
+
+                } else {
+
+                    if (pins[event.value] != '') {
+                        message(currentCall.callerID + 'has entered conference' + event.value + '.',
+                            {
+                                to: pins[event.value], network: 'SMS'
+                            });
+                    }
+                    say('<speak>Conference ID <say-as interpret-as="vxml:digits">' + event.value + '</say-as> accepted. You will now be placed into the conference. Press pound to exit without disconnecting. Please announce yourself.</speak>');
+
+                    conference(event.value, {terminator: "#"});
 
 
-    });
+                }
+            }
+        });
+
+    wait(1);
 }
